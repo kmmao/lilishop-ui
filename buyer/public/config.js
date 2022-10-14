15634950add97d61826f350bbc568f66b72d12ff
liukai <liukai@quanwu.info>
2022-10-14 15:14:18 +0800

    配置文件地址更新

--- buyer/public/config.js
+++ buyer/public/config.js
@@ -1,17 +1,25 @@
+/*
+ * @Author: liukai liukai@quanwu.info
+ * @Date: 2022-10-08 20:19:21
+ * @LastEditors: liukai liukai@quanwu.info
+ * @LastEditTime: 2022-10-13 02:06:18
+ * @FilePath: /lilishop-ui/buyer/public/config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 var BASE = {
     /**
      * @description api请求基础路径
      */
     API_DEV: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8889",
+      manager: "http://localhost:8887"
     },
     API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "https://common-api.zmshops.xycloud.info",
+      buyer: "https://buyer-api.zmshops.xycloud.info",
+      seller: "https://store-api.zmshops.xycloud.info",
+      manager: "https://admin-api.zmshops.xycloud.info"
     },
   };
