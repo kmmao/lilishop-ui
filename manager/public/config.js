627ac3a6df2c154a16600cc54ad5ede2ec03472c
liukai <liukai@quanwu.info>
2022-10-14 15:14:18 +0800

    配置文件地址更新

--- manager/public/config.js
+++ manager/public/config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: liukai liukai@quanwu.info
+ * @Date: 2022-10-08 20:19:21
+ * @LastEditors: liukai liukai@quanwu.info
+ * @LastEditTime: 2022-11-14 11:51:50
+ * @FilePath: /lilishop-ui/manager/public/config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 var BASE = {
     /**
      * @description api请求基础路径
@@ -9,19 +17,19 @@ var BASE = {
       manager: "https://admin-api.pickmall.cn"
     },
     API_PROD: {
-      common: "https://common-api.pickmall.cn",
-      buyer: "https://buyer-api.pickmall.cn",
-      seller: "https://store-api.pickmall.cn",
-      manager: "https://admin-api.pickmall.cn"
+      common: "https://common-api.zmshops.xycloud.info",
+      buyer: "https://buyer-api.zmshops.xycloud.info",
+      seller: "https://store-api.zmshops.xycloud.info",
+      manager: "https://admin-api.zmshops.xycloud.info"
     },
     /**
      * @description // 跳转买家端地址 pc端
      */
-    PC_URL: "https://pc-b2b2c.pickmall.cn",
+    PC_URL: "https://buyer.zmshops.xycloud.info",
     /**
      * @description  // 跳转买家端地址 wap端
      */
-    WAP_URL: "https://m-b2b2c.pickmall.cn",
+    WAP_URL: "https://h5.zmshops.xycloud.info",
     /**
      *  @description api请求基础路径前缀
      */
