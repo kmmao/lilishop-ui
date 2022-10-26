3f054822814186f126e5c5fd582359fe4c926616
liukai <liukai@quanwu.info>
2022-10-26 21:35:18 +0800

    管理端添加优惠券添加兑换嘟币值字段

--- manager/public/config.js
+++ manager/public/config.js
@@ -2,7 +2,7 @@
  * @Author: liukai liukai@quanwu.info
  * @Date: 2022-10-08 20:19:21
  * @LastEditors: liukai liukai@quanwu.info
- * @LastEditTime: 2022-10-09 00:12:08
+ * @LastEditTime: 2022-10-26 21:19:01
  * @FilePath: /lilishop-ui/manager/public/config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ var BASE = {
       common: "https://common-api.zmshops.xycloud.info",
       buyer: "https://buyer-api.zmshops.xycloud.info",
       seller: "https://store-api.zmshops.xycloud.info",
-      manager: "https://admin-api.zmshops.xycloud.info",
+      manager: "http://localhost:8887",
       //  manager: "http://192.168.0.120:8887",
       //  common: "http://192.168.0.120:8890",
 
